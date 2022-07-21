c03b0fdb330b454f3fe882e28f4c3df4d5db4b51
machinaa <i5046821854@github.com>
2022-07-21 17:51:40 +0900

    #9 - 도메인 코드를 엔티티로 변환
    1도메인클래스 : 1테이블 형식으로 매칭되도록 설계했다.
    생성자와 equals(), hashcode() 등이 도메인 맥락에 맞게 디테일하게 생성됨.

--- src/main/java/com/springExec/board/domain/Article.java
+++ src/main/java/com/springExec/board/domain/Article.java
@@ -1,17 +1,73 @@
 package com.springExec.board.domain;
 
-import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "title"),
+        @Index(columnList = "hashtag"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy"),
+})
+@EntityListeners(AuditingEntityListener.class)
+@Entity
 public class Article {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String title; //제목
-    private String content; //내용
-    private String hashtag; // 해시태그
 
-    private LocalDateTime createdAt; //생성일자
-    private String createdBy;  //생성자
-    private LocalDateTime modifiedAt; //수정일자
-    private String modifiedBy;  //수정자
+    @Setter @Column(nullable = false) private String title; //제목
+    @Setter @Column(nullable = false, length = 10000) private String content; //내용
 
+    @Setter private String hashtag; // 해시태그
+
+    @OrderBy("id")
+    @ToString.Exclude
+    @OneToMany(mappedBy = "article", cascade = CascadeType.ALL) private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
+
+    @CreatedDate @Column(nullable = false) private LocalDateTime createdAt; //생성일자
+    @CreatedBy @Column(nullable = false, length = 100) private String createdBy;  //생성자
+    @LastModifiedDate @Column(nullable = false) private LocalDateTime modifiedAt; //수정일자
+    @LastModifiedBy @Column(nullable = false, length = 100) private String modifiedBy;  //수정자
+
+    protected Article () {}
+
+    private Article(String title, String content, String hashtag) {
+        this.title = title;
+        this.content = content;
+        this.hashtag = hashtag;
+    }
+
+    //팩토리 메소드로서 생성자 구현
+    public static Article of(String title, String content, String hashtag) {
+        return new Article(title, content, hashtag);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Article article = (Article) o;
+        return id != null && id.equals(article.id);   //id가 부여되지 않은 경우 (영속화 되기 전에는 동등성 검사 x)
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
