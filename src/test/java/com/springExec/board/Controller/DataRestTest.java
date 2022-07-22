bd63d6c1bd7f10101591dc9e7ece20dcc2db9734
machinaa <i5046821854@github.com>
2022-07-22 19:57:54 +0900

    #1 - Spring Data REST 테스트 제외
    비즈니스 로직이 아닌 Spring Data REST에서 제공하는 기능을 이용한 테스트이며, 통합 테스트로 이루어져 있어 무겁고 DB를 건드리고 있으므로 제외한다

--- src/test/java/com/springExec/board/Controller/DataRestTest.java
+++ src/test/java/com/springExec/board/Controller/DataRestTest.java
@@ -1,6 +1,7 @@
 package com.springExec.board.Controller;
 
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +17,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 //@WebMvcTest
+@Disabled("Spring Data Rest에 대한 테스트는 제외")
 @DisplayName("Data Rest API Test")
 @AutoConfigureMockMvc
 @SpringBootTest
