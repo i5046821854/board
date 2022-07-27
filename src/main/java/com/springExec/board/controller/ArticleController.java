380482e3a85efe83d9a48f4cbb020e0a73b59317
machinaa <i5046821854@github.com>
2022-07-28 02:05:30 +0900

    #17 - 게시판 컨트롤러 구현
    게시판 기본 조회 화면을 위한 컨트롤러 구현하고
    데이터 전달 완료
    게시판 페이지 기본 정렬 속성 부여: 10개 1페이지, 시간 내림차순

--- src/main/java/com/springExec/board/controller/ArticleController.java
+++ src/main/java/com/springExec/board/controller/ArticleController.java
@@ -1,29 +1,44 @@
 package com.springExec.board.controller;
 
+import com.springExec.board.domain.type.SearchType;
+import com.springExec.board.response.ArticleResponse;
+import com.springExec.board.response.ArticleWithCommentsResponse;
+import com.springExec.board.service.ArticleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RequestMapping("/articles")
+@RequiredArgsConstructor
 @Controller
 public class ArticleController {
 
+    private final ArticleService articleService;
+
     @GetMapping
-    public String articles(ModelMap map)
+    public String articles(
+            @RequestParam(required = false) SearchType searchType,
+            @RequestParam(required = false) String searchValue,
+            @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC ) Pageable pageable,
+            ModelMap map)
     {
-        map.addAttribute("articles", List.of());
+        map.addAttribute("articles", articleService.searchArticles(searchType, searchValue, pageable).map(ArticleResponse::from));  //dto를 response 형태로 바꿔서 리턴
         return "articles/index";
     }
 
     @GetMapping("/{articleId}")
-    public String article(ModelMap map, @PathVariable String articleId)
+    public String article(ModelMap map, @PathVariable Long articleId)
     {
-        map.addAttribute("article", "article");
-        map.addAttribute("articleComments", List.of());
+        ArticleWithCommentsResponse article = ArticleWithCommentsResponse.from(articleService.getArticle(articleId));
+        map.addAttribute("article", article);
+        map.addAttribute("articleComments", article.articleCommentResponses());
 
         return "articles/detail";
     }
